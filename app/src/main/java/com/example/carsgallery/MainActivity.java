b32d1404e816086a16258022fc3cab36445657da
ahmedattia77 <max.kali.1212@gmail.com>
2023-02-23 01:28:43 +0200

    add activities , values

--- app/src/main/java/com/example/carsgallery/MainActivity.java
+++ app/src/main/java/com/example/carsgallery/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.carsgallery;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.view_car_activity);
+
+
     }
 }
